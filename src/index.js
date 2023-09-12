32f394a663b2dbd7cfcbe1a2b88d25530672960e
Georgiy95 <urijpetrovskij@gmail.com>
2023-09-12 15:30:42 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import ReactDOM from 'react-dom/client'; // бібліотека для рендерингу
+import App  from 'components/App';
+import './index.css'; // імпорт стилів
 
+// рендеринг компонента App в DOM-елемент з id root
 ReactDOM.createRoot(document.getElementById('root')).render(
+
+  // використання StrictMode для відлагодження
   <React.StrictMode>
     <App />
   </React.StrictMode>
